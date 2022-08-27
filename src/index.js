148fe4151449253757fbdaf688d4ee1e7403e992
e.baxter@dark-coin.io <e.baxter@dark-coin.io>
2022-08-26 22:11:54 -0500

    Adding example of generating a list of items from an array.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
+
+const myArray = ['apple', 'banana', 'orange'];
+
+const myList = myArray.map((item) => <p>{item}</p>)
+
+ReactDOM.render(myList, document.getElementById('root'));
